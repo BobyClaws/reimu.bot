badcd2b2d3cb436421647d70bcdad7fc745d4b47
Boby <bobyclaws@yandex.com>
2021-02-11 23:13:37 +0530

    added logging feature for Say.js

--- src/modules/Say.js
+++ src/modules/Say.js
@@ -1,5 +1,8 @@
 const Module = require("../core/Module");
-const RBot = require('../core/RBot');
+const RBot = require("../core/RBot");
+const Discord = require("discord.js");
+
+const confLoader = require("../util/confLoader");
 
 class Say extends Module {
 
@@ -18,20 +21,45 @@ class Say extends Module {
      * @param {string[]} args
      */
     processCommand(msg, args) {
-        msg.delete({ timeout: 0 });
-        let s = "";
-        for(let arg of args) s += arg + " ";
 
-        msg.channel.send(s);
-
-    
+        // log subcommand
+        if(args[0] == "log") {
+            let logs = confLoader.load("../data/Say/log.yml");
+            logs = logs.slice(0,5);
+            let message = "";
+            for(let log of logs)
+                message += "<@" + log.requested_by + ">  *" + log.message.trim() + "*\n\n";
+            let embed = new Discord.MessageEmbed();
+            embed.setColor("#dd2e44");
+            embed.setDescription(message);
             
+            msg.channel.send(embed);
+            return;
+            // TODO: return should indicate success.
+        } 
+
+        // default behaviour
+        msg.delete({ timeout: 0 });
+        let message = "";
+        for(let arg of args) message += arg + " ";
+        msg.channel.send(message);
+        this.recordLog(msg.author.id, message);
+    
+      
     }
 
     processInteraction(interaction) {
 
         let channel = this.rbot.dClient.channels.cache.get(interaction.channel_id);
-        channel.send(interaction.data.options[0].value);
+        let msg = interaction.data.options[0].value;
+        channel.send(msg);
+        this.recordLog(interaction.member.user.id, msg);
+    }
+
+    recordLog(username, message) {
+        let logs = confLoader.load("../data/Say/log.yml");
+        logs.unshift({"requested_by": username, "message": message});
+        confLoader.save("../data/Say/log.yml", logs.slice(0,5));
     }
 
 
