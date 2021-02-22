9a3dd1bf2298b31a2cf6d09ed1f9acd09f3190f3
Boby <bobyclaws@yandex.com>
2021-02-22 20:38:33 +0530

    updated working directory for slice

--- src/modules/Slice.js
+++ src/modules/Slice.js
@@ -25,12 +25,17 @@ class Slice extends Module {
         let member = msg.mentions.members.first();
         let url = member.user.displayAvatarURL();
         
+        // convert .webpg and .jpg urls to .png
         if(url.endsWith(".webp")) {
             url = url.slice(0,-5) + ".png";
+        
+        } else if(url.endsWith(".jpg")) {
+            url = url.slice(0,-4) + ".png";
         }
 
+
         this.log("got url:", url);
-        fs.writeFileSync("all/source/source_url.txt", url);
+        fs.writeFileSync("../external/Achievements/source/source_url.txt", url);
 
         exec("./all/process.sh",
             {shell: "/bin/bash"},
@@ -39,7 +44,7 @@ class Slice extends Module {
                 if (stderr) console.log(stderr);
                 if (stdout) console.log(stdout);
 
-                msg.channel.send("here", {files: ["all/out/final.gif"]});
+                msg.channel.send("here", {files: ["../external/Achievements/out/final.gif"]});
                 
 
             }
